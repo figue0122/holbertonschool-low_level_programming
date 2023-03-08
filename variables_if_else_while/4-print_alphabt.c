b3bc315053aed6761daf4a262755ce2ed75bdedd
Miguel F <miguel.figueroa0122@gmail.com>
2023-03-08 13:26:29 +0000

    print a-z w/o e/q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Main - Print the alphabet w/o q and e.
+ *
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ab;
+
+	for(ab = 'a'; ab <= 'z'; ab++)
+	if (ab != 'e' && ab != 'q')
+	putchar(ab);
+	putchar('\n');
+	
+	return (0);
+}
