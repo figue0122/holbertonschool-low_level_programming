0dfb52297b046dceaa5948b84db3fbea0b6d2f05
Miguel F <miguel.figueroa0122@gmail.com>
2023-03-08 13:38:42 +0000

    fixed typo

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * Main - Print alphabet in lowercase and uppercase
+ * main - Print alphabet in lowercase and uppercase
  *
  *
  * Return: Always 0
  */
+
 int main(void)
 {
 	char Az;
 	for (Az = 'a'; Az <= 'z'; Az++)
 	putchar(Az);
-
 	for (Az = 'A'; Az <= 'Z'; Az++)
 	putchar (Az);
 	putchar ('\n');
