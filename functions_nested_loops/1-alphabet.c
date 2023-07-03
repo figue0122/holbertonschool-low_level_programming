9fb488b9cfebe922935223205f4de6e0cd2e869d
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-03 13:45:15 +0000

    fix file

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet lower case
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	_putchar(letter);
+	letter++;
+
+	}
+	_putchar('\n');
+}
+
