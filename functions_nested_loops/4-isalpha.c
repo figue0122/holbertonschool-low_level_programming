c4e0d89d729454fd6ee23710a40932cf09ee66b6
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-12 18:48:11 +0000

    created a funtion that checks for abc char

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _isalpha - funt that looks for alphabeti char
+ * _isalpha - funtion that looks for alphabet char
  *
  * @c: variable
- * Return: 0
- **/
+ * Return: 0 otherwise
+ */
 
 int _isalpha(int c)
 {
 	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-
-			return (1);
-
+		return (1);
 	else
 		return (0);
 }
