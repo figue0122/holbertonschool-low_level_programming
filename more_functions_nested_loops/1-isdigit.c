431e34349f68ff800a11acb67fc3f6ad3864e3d0
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-14 21:47:11 +0000

    use function isdigit to check for digit 0-9

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _isdigit - a function that checks for the lowercase character
+ *
+ * @c: variable
+ *  Return: 1 and 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}i
