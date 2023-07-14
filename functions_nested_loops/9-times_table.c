770e1e031230d57ace0bd6994855e81839ecc124
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-14 15:06:32 +0000

    fix file

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,33 +6,16 @@
 
 void times_table(void)
 {
-	int a, b, c;;
+	int i, j, rslt;
 
-	for (a = 0; a <= 9; a++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar('0');
-                _putchar(',');
-                _putchar(' ');
-	for (b = 1; b <= 9; b++)
+	for (j = 0; j <= 9; j++)
 	{
-		c = (a * b);
-
-		if ((c / 10) > 0)
-		{
-			_putchar((c / 10) + '0');
-		}
-		else
-		{
-			_putchar(' ');
-		}
-			_putchar((c % 10) + '0');
-
-			if (b < 9)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		}
-			_putchar('\n');
-			}
+	rslt = i * j;
+	    printf("%d ", rslt);
+	}
+	printf("\n");
+	}
 }
+
