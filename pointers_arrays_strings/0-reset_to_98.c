c505fedbdf3eaf1cc60e9d69d9c103e2725af412
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-19 01:25:51 +0000

    changed ptr value to 98'

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
- * Changed var value 
- * using a pointer
+ * reset_to_98 - that takes a pointer to an int as parameter
+ * @n: variable
  *
  */
-
-void reset_to_98(int *n);
-{ 
+void reset_to_98(int *n)
+{
 	*n = 98;
 }
