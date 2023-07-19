1c3ff7b5ccd1b09f3503f8128290263ea03062dd
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-19 00:58:32 +0000

    print num between 0-9 expt 2,4

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  print_numbers - multiplies two integers
+ *
+ */
+void print_numbers(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		_putchar(n);
+		n++;
+	}
+		_putchar('\n');
+
+}
