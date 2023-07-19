9ec6a653b36aa065c0fee759e3ab6acfad95cdde
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-19 06:20:10 +0000

    reverse string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: variable
+ */
+void rev_string(char *s)
+{
+	int i, j, l, temp;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	l = i - 1;
+
+	while (j < l)
+	{
+		temp = s[j];
+		s[j] = s[l];
+		s[l] = temp;
+		j++;
+		l--;
+	}
+}
