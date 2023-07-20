d31db7c79b705629c583ec0e252a079440c819fc
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-20 18:30:24 +0000

    free the dogs

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,19 +1,18 @@
 #include "dog.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * free_dog - free all the dogs using 'free'stmnt
  *@d:dog_t pointer
  *
  */
 
-void free_dog(dog_t *d);
+void free_dog(dog_t *d)
 {
 	if (d != NULL)
 	{
-		free(d->name)
-		free(d->age)
-		free(d->owner)
-		free(d)
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
 }
