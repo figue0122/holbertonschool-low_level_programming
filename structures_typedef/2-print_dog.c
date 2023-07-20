1a84a1d74186ac5101ca390344fc33950d0b8a9c
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-20 17:24:16 +0000

    printed a struct

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
-		printf("Name: %s\n", d->owner);
+		printf("Name: %s\n", d->name);
 
 	printf("Age: %f\n", d->age);
 
