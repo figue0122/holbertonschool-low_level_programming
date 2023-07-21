23adeb799ffaa52357318528051b93a5de5a8a4c
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-21 18:53:25 +0000

    fix file

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ *array_interractor - Writes a fun that executes a func
+ *@array; int
+ *@action: int
+ *size@ size_t
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL && action == NULL)
+	{
+		for (i = 0; i < size; i++)
+	
+			action(array[i]);
+	}	
+}
