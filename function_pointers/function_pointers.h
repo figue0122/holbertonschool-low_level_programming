2f15051e8d3876ddd14a776a40cc8872ad129df2
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-21 18:15:08 +0000

    write a fun that prints a name

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
 #include <stddef.h>
 #include <stdlib.h>
-#ind
-#define
+#ifndef function_pointers_h
+#define function_pointers_h
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
