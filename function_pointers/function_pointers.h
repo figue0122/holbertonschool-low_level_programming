6901d6bc9d7c0f169b26e16d40807e8252893b11
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-21 18:35:48 +0000

    fsa

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#include <stddef.h>
-#include <stdlib.h>
 #ifndef function_pointers_h
 #define function_pointers_h
+#include <stddef.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
