5bbbda65ad1f228c92c43bcff29b6012215103a1
figue0122 <miguel.figueroa0122@gmail.com>
2023-08-01 18:59:24 +0000

    changed main.c and fix typo

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,31 @@
 #include "main.h"
+
 /**
- * _strncat - concatenates two strings using a number of bytes.
- * @dest: destination
- * @src: source
- * @n: variable
- * Return: dest
+ * _strncat - cats two strings
+ * Return: pointer to dest
+ * @dest: var
+ * @src: var
+ * @n: var
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j;
+	int a, b;
 
-	while (dest[i] != '\0')
-		i++;
-	for (j = 0; j < n && src[j] != '\0'; j++)
-		dest[j + i] = src[i];
+	for (a = 0 ; dest[a] != '\0' ; a++)
+	{
+	}
 
+	for (b = 0; src[b] != '\0' && b < n ; b++)
+	{
+		dest[a] = src[b];
+		a++;
+	}
+
+	if (b < n)
+	{
+		dest[a] = '\0';
+	}
 	return (dest);
+
 }
