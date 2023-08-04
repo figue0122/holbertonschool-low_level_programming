a1d11199dec2dc637fb43a29e99fa28ac41e89b2
figue0122 <miguel.figueroa0122@gmail.com>
2023-08-04 18:52:05 +0000

    func that return value x and y++

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * pow_recursion - function name
+ * _pow_recursion - function name
  * @x: int var
  * @y: int var
  * Return: int
@@ -9,11 +9,11 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (x < 0)
+	if (y < 0)
 		return (-1);
 
 	if (y == 0)
 		return (1);
 
-	return (x * _pow_recursion(x,y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
