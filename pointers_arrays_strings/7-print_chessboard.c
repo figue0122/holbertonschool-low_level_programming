345a3b447526f75ad8cb2306feeb6c35c2845668
figue0122 <miguel.figueroa0122@gmail.com>
2023-08-04 17:37:10 +0000

    used putchar to print

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -9,12 +9,17 @@ void print_chessboard(char (*a)[8])
 {
 	int x, y;
 
-	for (x = 0 ; x < 8 ; y++)
+	for (x = 0 ; x < 8 ; x++)
 	{
-		if (y == 7)
+		for (y = 0 ; y < 8 ; y++)
 		{
+			if (y == 7)
+			{
+				_putchar(a[x][y]);
+				_putchar('\n');
+			}
+			else
 			_putchar(a[x][y]);
-			_putchar('\n');
 		}
 	}
 }
