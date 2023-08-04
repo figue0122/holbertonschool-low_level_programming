f725035e0b9b4fb12dc02eea303cd865a262fb49
figue0122 <miguel.figueroa0122@gmail.com>
2023-08-04 16:50:47 +0000

    memory function

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - memory function
+ * @s: char ->
+ * @b: char var
+ * @n: unsigned int var
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		s[1] = b;
+	}
+	return (s);
+}
