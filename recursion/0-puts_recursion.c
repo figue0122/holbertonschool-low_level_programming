618120b9cea5b717d4f2e7a79af20778b318b0c0
figue0122 <miguel.figueroa0122@gmail.com>
2023-08-04 18:21:02 +0000

    puts with recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * printed a string using puts
- * Always 0.
+ * _puts_recursion - function name
+ * @s: pointer
  */
 
-void _puts_recursion(char *s) {
-	if (*s != '\0') {
-	putchar(*s);
-	_puts_recursion(s + 1);
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
 	}
-    else
-    {
-        putchar('\n');
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
