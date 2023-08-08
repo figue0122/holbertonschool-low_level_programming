c158becc052212da38efcd59225498d28466393c
figue0122 <miguel.figueroa0122@gmail.com>
2023-08-08 15:12:46 +0000

    # of argv counter

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: int var
+ * @argv: char pinter array var
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
