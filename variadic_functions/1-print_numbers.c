75b3c238a0e20b9b98d982b0c78f7cf9839a068a
figue0122 <120404359+figue0122@users.noreply.github.com>
2023-08-17 12:37:35 -0400

    fix

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function name
+ * @separator: char ->
+ * @n: unsigned int
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (n > 0)
+	{
+		va_start(args, n);
+		while (i < n)
+		{
+			printf("%d", va_arg(args, int));
+			if (i != n - 1 && separator != NULL)
+			{
+				printf("%s", separator);
+			}
+			i++;
+		}
+		va_end(args);
+	}
+	printf("\n");
+}
