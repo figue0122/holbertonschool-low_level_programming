e030832b4fbca8a6320729d12b560fe21069185c
figue0122 <miguel.figueroa0122@gmail.com>
2023-08-26 16:48:20 +0000

    fixing betty and wrong var

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,39 +1,45 @@
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - insert a node at given location
- * @h: head
- * @idx: index
- * @n: data
- * Return: address of new node
+ * insert_dnodeint_at_index - function that insert a new node at given position
+ * @h: pointer to the start of the list
+ * @idx: given position
+ * @n: data to give to the new node
+ * Return: the addres of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new;
+	dlistint_t *new_node, *prev_node, *tmp = *h;
+	unsigned int i = 0;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	for (; idx != 1; idx--)
-	{
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
-	}
-
-	if (tmp->next == NULL)
-		return (add_dnodeint_end(h, n));
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
 		return (NULL);
-
-	new->n = n;
-	new->prev = tmp;
-	new->next = tmp->next;
-	tmp = new;
-	tmp->next = new;
-
-	return (new);
+	if (!h)
+		return (NULL);
+	while (tmp)
+		tmp = tmp->next, i++;
+	if (idx > i)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+	tmp = *h;
+	for (i = 0; i < idx; i++)
+	{
+		prev_node = tmp;
+		tmp = tmp->next;
+	}
+	new_node->next = tmp;
+	new_node->prev = prev_node;
+	prev_node->next = new_node;
+	if (tmp)
+		tmp->prev = new_node;
+	return (new_node);
 }
